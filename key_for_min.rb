b810935ab29113f32f7bfefa6009f7146768ff7e
Andy Maskiell <maskiella@gmail.com>
2017-12-11 20:56:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,14 @@
 def key_for_min_value(name_hash)
   min_key = ""
   name_hash.each do |key, value|
+    if value < (value[0] && value[1])
+      min_key << key
+    elsif value < (value[1] && value[2])
+      min_key << key
+    elsif value < (value[0] && value[2])
+      min_key << key
+    else
+      return nil
   end
   min_key
 end
